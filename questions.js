cddf1f8503445b9d9154d9378806a183bfffda76
marigoldy21 <75244215+marigoldy21@users.noreply.github.com>
2022-01-25 13:22:11 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the brightest planet in the night's sky?",
+    answer: "Venus",
+    options: [
+      "Jupiter",
+      "Venus",
+      "Mercury",
+      "Saturn"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is the second smallest planet within our solar system?",
+    answer: "Mars",
+    options: [
+      "Mars",
+      "Venus",
+      "Mercury",
+      "Pluto"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The moon called Titan orbits whuich planet?",
+    answer: "Saturn",
+    options: [
+      "Jupiter",
+      "Uranus",
+      "Neptune",
+      "Saturn"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Uranus has only been visited by what spacecraft?",
+    answer: "Voyager 2",
+    options: [
+      "Mariner 4",
+      "Apollo 11",
+      "Cassini",
+      "Voyager 2"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many of our planets can be seen without a telescope?",
+    answer: "5",
+    options: [
+      "7",
+      "3",
+      "5",
+      "4"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
